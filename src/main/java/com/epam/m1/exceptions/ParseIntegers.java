836b1bf6fc8fdcfca593d6ca6a4fe1b13f3c8b06
Zhan <jabberwork.owl@gmail.com>
2023-04-17 22:10:09 +0600

    s

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -9,20 +9,27 @@ import java.util.List;
  * space delimiter
  */
 public class ParseIntegers {
+
+  private static final List<String> WORDS =
+          Arrays.asList(
+                  "JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations."
+                          .split(" "));
+
   public static void main(String[] args) {
-    String[] WORDS = {"The", "answer", "is", "42"};
+    Iterator<String> words = WORDS.iterator();
     int sum = 0;
-    for (String word : WORDS) {
+    String justWords = "";
+    while (words.hasNext()) {
+      String next = words.next();
       try {
-        sum += Integer.parseInt(word);
+        int number = Integer.parseInt(next);
+        sum += number;
       } catch (NumberFormatException e) {
-        // ignore non-numeric words
+        justWords += next + " ";
       }
     }
-    String result = String.join(" ", WORDS);
-    System.out.println("Sum of numbers: " + sum);
-    System.out.println("Result: " + result);
+    System.out.println("Sum is " + sum);
+    System.out.println("Just words:" + justWords);
   }
-
 }
 

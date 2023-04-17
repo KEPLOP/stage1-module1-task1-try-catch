9f50d870eb12a5eb7abf225af4fb63bd952de6c9
Zhan <jabberwork.owl@gmail.com>
2023-04-17 19:41:35 +0600

    s

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -8,20 +8,21 @@ import java.util.List;
  * Complete the code, parse integers, calculate the sum of numbers in the WORDS, join strings with
  * space delimiter
  */
-public class ParseIntegers { 
+public class ParseIntegers {
   public static void main(String[] args) {
-    int sum = 0; 
-    String str = "When a humble bard, graced right along, with Geralt of Rivia along came this song123"; 
-    String[] WORDS = str.split(" "); 
-    for (String word : WORDS) { 
-      for (int i = 0; i < word.length(); i++) {
-        char ch = word.charAt(i); 
-        if (Character.isDigit(ch)) 
-        { int digit = Character.getNumericValue(ch); sum += digit; 
-        } 
-      } 
-    } 
-    System.out.println("Сумма цифр в строке " + "'" + str + "'" + " равна " + sum); 
-  } 
+    String[] WORDS = {"The", "answer", "is", "42"};
+    int sum = 0;
+    for (String word : WORDS) {
+      try {
+        sum += Integer.parseInt(word);
+      } catch (NumberFormatException e) {
+        // ignore non-numeric words
+      }
+    }
+    String result = String.join(" ", WORDS);
+    System.out.println("Sum of numbers: " + sum);
+    System.out.println("Result: " + result);
+  }
+
 }
 
